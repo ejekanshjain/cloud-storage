786e0073701b2044735930049a0717b116553ae2
Ekansh Jain <ejekanshjain@gmail.com>
2023-07-20 13:32:00 +0530

    add test for r2 and firebase

--- src/test.ts
+++ src/test.ts
@@ -29,6 +29,27 @@ const testS3 = async () => {
   await s3Client.deleteFile(testData.filename)
 }
 
+const testFirebaseStorage = async () => {
+  const firebaseStorageClient = new GCPStorageClient({
+    projectId: process.env.FIREBASE_PROJECT_ID!,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY!,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
+    bucket: process.env.FIREBASE_STORAGE_BUCKET!
+  })
+
+  await firebaseStorageClient.addFile(
+    {
+      filename: testData.filename,
+      data: testData.data
+    },
+    true
+  )
+
+  await firebaseStorageClient.getFile(testData.filename)
+
+  await firebaseStorageClient.deleteFile(testData.filename)
+}
+
 const testGCPStorage = async () => {
   const gcpStorageClient = new GCPStorageClient({
     projectId: process.env.GCP_PROJECT_ID!,
@@ -66,10 +87,31 @@ const testAzureStorage = async () => {
   await azureStorageClient.deleteFile(testData.filename)
 }
 
+const testR2 = async () => {
+  const r2Client = new S3Client({
+    region: process.env.R2_REGION!,
+    accessKey: process.env.R2_ACCESS_KEY!,
+    accessSecret: process.env.R2_ACCESS_SECRET!,
+    bucket: process.env.R2_BUCKET!,
+    host: process.env.R2_HOST!
+  })
+
+  await r2Client.addFile({
+    filename: testData.filename,
+    data: testData.data
+  })
+
+  await r2Client.getFile(testData.filename)
+
+  await r2Client.deleteFile(testData.filename)
+}
+
 const main = async () => {
   await testS3()
+  await testFirebaseStorage()
   await testGCPStorage()
   await testAzureStorage()
+  await testR2()
 }
 
 main()
